e28823b409cdcfdccfb47668172650a13801f9c2
RenatZvezdkin <itzstardusty@gmail.com>
2023-05-14 16:50:38 +0300

    Доведение до ума функций удаления и загрузки, а также отображения файлов из базы данных. Из оставшихся ошибок - выбор папки для сохранения файлов

--- CrossplatformRadioApp/ViewModels/FilesPageViewModel.cs
+++ CrossplatformRadioApp/ViewModels/FilesPageViewModel.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Controls.Selection;
 using Avalonia.Platform.Storage;
 using CrossplatformRadioApp.Models;
+using DynamicData;
 using MessageBox.Avalonia.Enums;
 using ReactiveUI;
 
@@ -12,25 +16,10 @@ namespace CrossplatformRadioApp.ViewModels
 {
     class FilesPageViewModel : ViewModelBase, INotifyCollectionChanged
     {
-        private FilePickerSaveOptions _saveFileDialog;
-        private FilePickerOpenOptions _openFileDealog;
-        public ObservableCollection<FileModel> FileModels { get; }
-        public List<FileModel>? _selectedFileModels;
-        public List<FileModel>? SelectedFileModels
-        {
-            get => _selectedFileModels;
-            set
-            {
-                _selectedFileModels = value;
-                SaveToDirCommand.RaiseCanExecuteChanged();
-                DeleteCommand.RaiseCanExecuteChanged();
-            }
-        }
-        public RelayCommand DeleteCommand { get; }
-        public RelayCommand SaveToDirCommand { get; }
-        public RelayCommand AddFileToDbCommand { get; }
         public FilesPageViewModel()
         {
+            SelectModel = new SelectionModel<FileModel>();
+            SelectModel.SelectionChanged += SelectedItemsChangeEvent;
             FileModels = new ObservableCollection<FileModel>(FileModel.GetFileModelsFromDatabase());
             _saveFileDialog = new FilePickerSaveOptions
             {
@@ -42,36 +31,48 @@ namespace CrossplatformRadioApp.ViewModels
                 AllowMultiple = true
             };
             FileModels.CollectionChanged += CollectionChanged;
-            AddFileToDbCommand = new RelayCommand(o =>
+            AddFileToDbCommand = new RelayCommand(async o =>
             {
-                using var window = Manager.Instance.MainWindow.StorageProvider.OpenFilePickerAsync(_openFileDealog);
-                window.Wait();
-                FileModel.WriteMultipleFilesIntoDatabase(
-                    window.Result.
-                        Select(storageFile => storageFile?.TryGetLocalPath()).
+                var result = await Manager.Instance.MainWindow.StorageProvider.
+                    OpenFilePickerAsync(_openFileDealog);
+                var addedFileModels = FileModel.WriteMultipleFilesIntoDatabase(
+                    result.
+                        Select(storageFile => storageFile.TryGetLocalPath()).
                         Where(path => !string.IsNullOrWhiteSpace(path)));
+                FileModels.AddRange(addedFileModels);
             });
-            SaveToDirCommand = new RelayCommand(o =>
+            SaveToDirCommand = new RelayCommand(async o =>
             {
-                using var window = Manager.Instance.MainWindow.StorageProvider.SaveFilePickerAsync(_saveFileDialog);
-                window.Wait();
-                var localPath = window.Result?.TryGetLocalPath();
-                if (SelectedFileModels.Any() && !string.IsNullOrWhiteSpace(localPath))
+                var localPath = (await Manager.Instance.MainWindow.StorageProvider.
+                        SaveFilePickerAsync(_saveFileDialog))?.
+                    TryGetLocalPath();
+                if (AnyModelsSelected && !string.IsNullOrWhiteSpace(localPath))
                     FileModel.SaveMultipleFileModelsToDirectory(SelectedFileModels, localPath);
             },o => AnyModelsSelected);
-            DeleteCommand = new RelayCommand(o =>
+            DeleteCommand = new RelayCommand(async o =>
             {
-                var task = MessageBox.Avalonia.MessageBoxManager
-                    .GetMessageBoxStandardWindow("Удаление", "Вы уверены, что хотите выбранные файлы")
-                    .ShowDialog(Manager.Instance.MainWindow);
-                task.Wait();
-                if (task.Result == ButtonResult.Yes)
-                    FileModel.DeleteMultipleFilesFromDatabase(_selectedFileModels, FileModels);
+                using var task = MessageBox.Avalonia.MessageBoxManager.
+                    GetMessageBoxStandardWindow("Удаление", "Вы уверены, что хотите удалить выбранные файлы?", ButtonEnum.YesNo).
+                    ShowDialog(Manager.Instance.MainWindow);
+                if (await task == ButtonResult.Yes)
+                    FileModel.DeleteMultipleFilesFromDatabase(SelectedFileModels, FileModels);
             },o => AnyModelsSelected);
-            SelectedFileModels = null;
         }
-        private bool AnyModelsSelected => SelectedFileModels != null && SelectedFileModels.Any();
-
+        private FilePickerSaveOptions _saveFileDialog;
+        private FilePickerOpenOptions _openFileDealog;
+        public ObservableCollection<FileModel> FileModels { get; }
+        public RelayCommand DeleteCommand { get; }
+        public RelayCommand SaveToDirCommand { get; }
+        public RelayCommand AddFileToDbCommand { get; }
         public event NotifyCollectionChangedEventHandler? CollectionChanged;
+        public SelectionModel<FileModel> SelectModel { get; }
+        private bool AnyModelsSelected => SelectModel.SelectedItems.Any();
+        public List<FileModel> SelectedFileModels => SelectModel.SelectedItems.ToList();
+
+        void SelectedItemsChangeEvent(object? sender, SelectionModelSelectionChangedEventArgs e)
+        {
+            SaveToDirCommand.RaiseCanExecuteChanged();
+            DeleteCommand.RaiseCanExecuteChanged();
+        }
     }
 }
